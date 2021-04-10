059cfb255558e6052849dc0c3a6f424314624ea9
kaleakash <akash300383@gmail.com>
2021-04-10 12:15:18 +0530

    @ViewChild

--- Angular Projects/angular-component-communication/src/app/app.component.ts
+++ Angular Projects/angular-component-communication/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Child1Component } from './child1/child1.component';
+import { Child2Component } from './child2/child2.component';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +13,26 @@ export class AppComponent {
   
   childAge?:number;
   
+  @ViewChild(Child2Component)   // inject the reference of child2 component
+  child2?:Child2Component;
+
+  childDesg?:string;
+
   displayName(name:any){
     this.parentName=name;
   }
   addProduct(product:string){
     this.products.push(product);
+  }
+
+  fun(event:any){
+    console.log(event)
+    console.log(event.target.name)
+    console.log(event.target.value)
+  }
+
+  callChild2Fun() {
+    this.child2?.child2Fun();
+    this.childDesg=this.child2?.desg;
   }
 }
--- Angular Projects/angular-component-communication/src/app/child2/child2.component.ts
+++ Angular Projects/angular-component-communication/src/app/child2/child2.component.ts
@@ -8,9 +8,15 @@ import { Component, Input, OnInit } from '@angular/core';
 export class Child2Component implements OnInit {
   @Input()
   productDetails?:Array<string>;
+  
+  desg:string="Tester";
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  child2Fun() {
+    console.log("Child2 function");
+  }
 }
